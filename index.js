f8f738638487232841dcfead981bef799341417f
Kenny J <k-tech19@Kennys-Mac-mini.local>
2021-05-14 15:05:48 -0400

    #14

--- index.js
+++ index.js
@@ -340,3 +340,13 @@ function howManyLightsabersDoYouOwn(name) {
 
 // Solution:
 
+function smallEnough(a, limit){
+  
+
+    for(let i = 0; i < a.length; i++) {
+      if(a[i] > limit ){
+        return false
+      } 
+    } 
+    return true
+  }
