21258480a040f7eb372740411d209abec729a03c
Kenny J <k-tech19@Kennys-Mac-mini.local>
2021-05-14 12:12:38 -0400

    batch of problem #4

--- index.js
+++ index.js
@@ -171,3 +171,163 @@ function makeUpperCase(str) {
 // The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
 
 // Solution: 
+
+function gooseFilter (birds) {
+    let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    
+    // return an array containing all of the strings in the input array except those that match strings in geese
+    
+    let removeGeese = [];
+    
+    for(i = 0; i < birds.length; i++ ){
+      if(!geese.includes(birds[i])){
+         removeGeese.push(birds[i])
+        }
+    }
+    
+     
+      return removeGeese
+    
+  };
+
+//   8 kyu
+// Will you make it?
+
+// Will you make it?
+// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left. Considering these factors, write a function that tells you if it is possible to get to the pump or not. Function should return true (1 in Prolog) if it is possible and false (0 in Prolog) if not. The input values are always positive.
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  
+    // Nearest pump 50 miles
+    const d = distanceToPump;
+    // on average car runs 25 per miles hour
+    let m = mpg;
+    // 2 gallons left
+    let f = fuelLeft;
+      if (d <= f * m){
+        return true
+      } else {
+        return false
+      }
+    //should return true 
+    //should return false 
+    // values are positive
+    
+  };
+
+
+//   8 kyu
+// Area or Perimeter
+
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+// area_or_perimeter(6, 10) --> 32
+// area_or_perimeter(3, 3) --> 9
+// Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+
+const areaOrPerimeter = function(l , w) {
+    return l == w ? l*w : l*2 + w*2 ; 
+    
+  //    if (l == w) {
+  //        return l*w 
+  //   } else {
+  //     return l*2 + w*2
+  //   }
+  };
+
+//   8 kyu
+// The Wide-Mouthed frog!
+
+// The wide mouth frog is particularly interested in the eating habits of other creatures.
+
+// He just can't stop asking the creatures he encounters what they like to eat. But then he meet the alligator who just LOVES to eat wide-mouthed frogs!
+
+// When he meets the alligator, it then makes a tiny mouth.
+
+// Your goal in this kata is to create complete the mouth_size method this method take one argument animal which corresponds to the animal encountered by frog. If this one is an alligator (case insensitive) return small otherwise return wide.
+
+// Solution: 
+
+function mouthSize(animal) {
+  
+    let a = animal
+    
+    let alligator = "small"
+    let w = "wide"
+    
+    if (a == "alligator" ){
+      return alligator
+    } else {
+      return w
+      }  
+    
+    }
+
+//     8 kyu
+// Calculate BMI
+
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+// Solution: 
+
+function bmi(weight, height) {
+  
+    let bmi = weight/height**2;
+  
+    let u = 'Underweight'
+    let normal = 'Normal'
+    let overweight = 'Overweight'
+    let obese = 'Obese'
+    
+      if( bmi <= 18.5){
+        return u
+      } else if( bmi <= 25) {
+        return normal
+      } else if ( bmi <= 30) {
+        return overweight
+      } else if ( bmi > 30 ) {
+        return obese
+      }
+    
+  }
+
+//   8 kyu
+// How many lightsabers do you own?
+
+// Inspired by the development team at Vooza, write the function that
+
+// accepts the name of a programmer, and
+// returns the number of lightsabers owned by that person.
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+// Note: your function should have a default parameter.
+
+// For example(Input --> Output):
+
+// "anyone else" --> 0
+// "Zach" --> 18
+
+// Solution:
+
+function howManyLightsabersDoYouOwn(name) {
+  
+    let n = name;
+    let z = 'Zach'
+  
+    if(n !== z ) {
+      return 0
+    } else { 
+    return 18
+    }
+    
+  }
+
