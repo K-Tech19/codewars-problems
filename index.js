773e116e81ca2f3a65b4f9d1715157011cff7930
Kenny J <k-tech19@Kennys-Mac-mini.local>
2021-05-17 14:31:16 -0400

    daily problem #15

--- index.js
+++ index.js
@@ -350,3 +350,42 @@ function smallEnough(a, limit){
     } 
     return true
   }
+
+
+  // 7 kyu
+  // Find the vowels
+
+  // We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+  // So given a string "super", we should return a list of [2, 4].
+  
+  // Some examples:
+  // Mmmm  => []
+  // Super => [2,4]
+  // Apple => [1,5]
+  // YoMama -> [1,2,4,6]
+  // NOTES
+  // Vowels in this context refers to: a e i o u y (including upper case)
+  // This is indexed from [1..n] (not zero indexed!)
+
+
+
+  // Solution:
+
+  function vowelIndices(word){
+    let vowel = ['a', 'e', 'i', 'o', 'u', 'y']
+    
+    let w = word
+    let v = []
+    
+    // convert word into lowerCase and then split them
+    
+    w = w.toLowerCase().split('')
+    
+    for( let i = 0; i < w.length; i++){
+      if(vowel.includes(w[i])){
+        v.push( i + 1)
+      }
+    }
+    return v
+  }
